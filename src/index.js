297be9a7181ba5f9defa2ca6002529e23d47eb9b
Mayank <mayankrajanand2001@gmail.com>
2024-01-13 00:15:42 +0530

    Custom error and successResponse class

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 import connectToDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Express unable to communicate with db", err);
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed !!!");
+  });
